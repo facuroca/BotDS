b79f5c35baf31106f684cb0832f5a8dd59976d02
facuroca <gas1nker@gmail.com>
2024-07-14 02:43:50 -0300

    tweaks

--- bot.js
+++ bot.js
@@ -21,6 +21,12 @@ client.login(process.env.DISCORD_BOT_TOKEN);
 //responder al primer mensaje recibido en el canal
 client.on(Events.MessageCreate, async (message) => {
     if (message.author.bot) return
+    if (message.content === 'facu') { 
+        message.reply('crack')
+    }
+    if (message.content === 'mariano') { 
+        message.reply('bondar')
+    }
     if (!message.content.startsWith('-')) return
 
     const args = message.content.slice(1).trim().split(' ')[0]
