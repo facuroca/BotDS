36be622740cc59dd951877e1f69183266b19ed2d
facuroca <gas1nker@gmail.com>
2024-07-17 17:40:01 -0300

    added tts feature command -need ttsmessages on and permissions

---
+++ commands/tts.js
@@ -0,0 +1,22 @@
+const {EmbedBuilder, SlashCommandBuilder, PermissionsBitField} = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("tts")
+        .setDescription("Envía un mensaje de texto a voz.")
+        .addStringOption((option) =>
+            option
+                .setName("texto")
+                .setDescription("Texto a enviar")
+                .setRequired(true)
+        ),
+    async execute(interaction) {
+        if(!interaction.member.permissions.has(PermissionsBitField.Flags.SendTTSMessages)) {
+            return interaction.reply({ content: "No tienes permisos para enviar mensajes de texto a voz." });
+        }
+        const text = interaction.options.getString("texto");
+
+
+        await interaction.reply({ content: `${text}` , tts: true , ephemeral: true });
+    },
+    };
