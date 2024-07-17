4190e672dce7359be7c2f53daa1d4e9217b1e92e
facuroca <gas1nker@gmail.com>
2024-07-16 21:18:41 -0300

    added better events handlers

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@
 require('dotenv').config();
 const {Client, Events, Collection} = require('discord.js'); //requerir discord.js
 const fs = require('fs'); //requerir fs
+const path = require('path'); //requerir path
 
 
 
@@ -10,66 +11,22 @@ const client = new Client({
     intents: 3276799
 });
 
-//definir colecciones
-client.commands = getCommands('./commands');
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for(const file of eventFiles) {
+    const event = require(path.join(eventsPath, file));
+    if(event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
 
 
 
-//contenido del bot
-client.on(Events.ClientReady, async () => {
-    console.log(`El bot esta conectado como ${client.user.username}`);
-});
 
 //conectar el bot
 client.login(process.env.DISCORD_BOT_TOKEN);
 
-//responder al primer mensaje recibido en el canal
-client.on(Events.MessageCreate, async (message) => {
-    if (message.author.bot) return
-    if (!message.content.startsWith('-')) return
-    const args = message.content.slice(1).trim().split(' ')[0]
-    try {
-        const command = require(`./commandsText/${args}`);
-        command.run(message);
-    } catch (error) {
-        console.log(`ha ocurrido un error al intentar responder el mensaje -${args}`, error.message);
-    }
-});
 
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isChatInputCommand()) return; 
-        const command = client.commands.get(interaction.commandName);
-
-        try {
-            if (interaction.replied) return;
-            await command.execute(interaction);
-        } catch (error) {
-            console.error(`Error al ejecutar el comando ${interaction.commandName}:`, error);
-        }
-});
-
-function getCommands(dir) {
-    let commands = new Collection();
-    const commandFiles = getFiles(dir);
-    for (const file of commandFiles) {
-        const command = require(file);
-        commands.set(command.data.toJSON().name, command);
-    }
-    return commands;
-}
-function getFiles(dir) {
-    const files = fs.readdirSync(dir, { withFileTypes: true });
-    let commandFiles = [];
-
-    for (const file of files) {
-        if (file.isDirectory()) {
-            commandFiles = [
-                ...commandFiles,
-                ...getFiles(`${dir}/${file.name}`) // Use path.join for cross-platform compatibility
-            ];
-        } else if (file.name.endsWith(".js")) {
-            commandFiles.push(`${dir}/${file.name}`); // Use path.join here as well
-        }
-    }
-    return commandFiles;
-}
---
+++ events/interactionCreate.js
@@ -0,0 +1,47 @@
+const {Client, Collection} = require('discord.js');
+const fs = require('fs');
+const client = new Client({intents: 3276799});
+
+//definir colecciones
+client.commands = getCommands('./commands');
+
+module.exports = {
+    name: "interactionCreate",
+    async execute(interaction) {
+        if (!interaction.isChatInputCommand()) return; 
+        const command = client.commands.get(interaction.commandName);
+
+        try {
+            if (interaction.replied) return;
+            await command.execute(interaction);
+        } catch (error) {
+            console.error(`Error al ejecutar el comando ${interaction.commandName}:`, error);
+        }
+    },
+};
+
+function getCommands(dir) {
+    let commands = new Collection();
+    const commandFiles = getFiles(dir);
+    for (const file of commandFiles) {
+        const command = require("."+file);
+        commands.set(command.data.toJSON().name, command);
+    }
+    return commands;
+}
+function getFiles(dir) {
+    const files = fs.readdirSync(dir, { withFileTypes: true });
+    let commandFiles = [];
+
+    for (const file of files) {
+        if (file.isDirectory()) {
+            commandFiles = [
+                ...commandFiles,
+                ...getFiles(`${dir}/${file.name}`) // Use path.join for cross-platform compatibility
+            ];
+        } else if (file.name.endsWith(".js")) {
+            commandFiles.push(`${dir}/${file.name}`); // Use path.join here as well
+        }
+    }
+    return commandFiles;
+}
