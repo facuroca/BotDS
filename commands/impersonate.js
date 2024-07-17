6b2f25daa1dd431a20a30a19b9a43ae1d27db6b6
facuroca <gas1nker@gmail.com>
2024-07-17 17:59:06 -0300

    added new impersonate command for fun!

---
+++ commands/impersonate.js
@@ -0,0 +1,34 @@
+const {SlashCommandBuilder} = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("impersonate")
+        .setDescription("Hacete pasar por otro usuario")
+        .addUserOption((option) =>
+            option
+                .setName("usuario")
+                .setDescription("El usuario que queres imitar")
+                .setRequired(true)
+        )
+        .addStringOption((option) =>
+            option
+                .setName("mensaje")
+                .setDescription("El mensaje que queres enviar")
+                .setRequired(true)
+        ),
+    async execute(interaction) {
+        const user = interaction.options.getUser("user");
+        const message = interaction.options.getString("message");
+        await interaction.channel.createWebhook({
+            name : user.username,
+            avatar : user.displayAvatarURL({dynamic : true})
+        }).then((webhook) => {
+            webhook.send({content: message});
+            setTimeout(() => {
+                webhook.delete();
+            }, 3000);
+            
+        });
+        await interaction.reply({content: "Acabo de imitar al usuario aca arriba!", ephemeral: true});
+    },
+};
